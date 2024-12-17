1e257b7717685e016506c38056a92149664e03a9
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-12-17 06:34:32 +0100

    Add sort data functions

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,59 +1,67 @@
 import { ContactsCollection } from '../db/models/contacts.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
+import { SORT_ORDER } from '../constants/index.js';
 
-export const getAllContacts = async ({ page, perPage }) => {
-    const limit = perPage;
-    const skip = (page - 1) * perPage;
-    
-    const contactsQuery = ContactsCollection.find();
-    const contactsCount = await ContactsCollection.find().merge(contactsQuery).countDocuments();
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  SORT_ORDER = SORT_ORDER.ASC,
+  sortBy = 'name',
+}) => {
+  const limit = perPage;
+  const skip = (page - 1) * perPage;
 
-    const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+  const contactsQuery = ContactsCollection.find();
+  const contactsCount = await ContactsCollection.find()
+    .merge(contactsQuery)
+    .countDocuments();
 
-    const paginationData = calculatePaginationData(contactsCount, perPage, page);
-    return {
-        data: contacts,
-        ...paginationData,
-    };
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
+
+  const paginationData = calculatePaginationData(contactsCount, perPage, page);
+  return {
+    data: contacts,
+    ...paginationData,
+  };
 };
-
-
-
-
 
 export const getContactById = async (contactId) => {
-    const contact = await ContactsCollection.findById(contactId);
-    return contact;
+  const contact = await ContactsCollection.findById(contactId);
+  return contact;
 };
 
-export const createContact = async (playload) => { 
-    const contact = await ContactsCollection.create(playload);
-    return contact;
+export const createContact = async (playload) => {
+  const contact = await ContactsCollection.create(playload);
+  return contact;
 };
 
 export const deleteContact = async (contactId) => {
-    const contact = await ContactsCollection.findOneAndDelete({
-        _id: contactId,
-    });
-    return contact;
+  const contact = await ContactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
+  return contact;
 };
 
 export const updateContact = async (contactId, payload, options = {}) => {
-    const rawResult = await ContactsCollection.findOneAndUpdate(
-        { _id: contactId },
-        payload,
-        {
-            new: true,
-            includeResultMetadata: true,
-            ...options,
-        },
-    );
+  const rawResult = await ContactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
 
-    if (!rawResult || !rawResult.value) return null;
+  if (!rawResult || !rawResult.value) return null;
 
-    return {
-      contact: rawResult.value,
-      isNew: Boolean(rawResult?.lastErrorObject?.upserted),
-    };
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
 };
