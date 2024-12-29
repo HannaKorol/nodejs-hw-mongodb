cfad9bcee83e1fd64a9c9a53551b79fc2507a345
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2024-12-29 17:09:20 +0100

    Add additional information to  updateContactSchema in joi schema

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,9 +1,13 @@
 import Joi from "joi";
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string().min(3).max(20),
-  phoneNumber: Joi.string().min(3).max(20),
+  name: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Username should be a string', // Кастомізація повідомлення для типу "string"
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+  }),
+  phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().min(3).max(20),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().min(3).max(20),
+  contactType: Joi.string().min(3).max(20).required(),
 });
