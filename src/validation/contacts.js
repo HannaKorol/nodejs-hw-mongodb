88c090437adf0276995ec5dbb32362ea1dbd8d8c
Hanna Korol <162054571+HannaKorol@users.noreply.github.com>
2025-01-02 11:12:27 +0100

    Edit deleteContactController to get an error message through "throw" and add a separate validation schema for patch( * without 'required'.

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,5 +1,7 @@
 import Joi from "joi";
 
+
+//Post
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required().messages({
     'string.base': 'Username should be a string', // Кастомізація повідомлення для типу "string"
@@ -11,3 +13,20 @@ export const updateContactSchema = Joi.object({
   isFavourite: Joi.boolean(),
   contactType: Joi.string().min(3).max(20).required(),
 });
+
+
+
+//Patch 
+export const modifyContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20).messages({
+    'string.base': 'Username should be a string', // Кастомізація повідомлення для типу "string"
+    'string.min': 'Username should have at least {#limit} characters',
+    'string.max': 'Username should have at most {#limit} characters',
+  }),
+  phoneNumber: Joi.string().min(3).max(20),
+  email: Joi.string().min(3).max(20),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().min(3).max(20),
+});
+
+
